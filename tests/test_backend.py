7e9809c0cc18dd83d24fc8f1201a03a8593d4b54
Just van Rossum <justvanrossum@gmail.com>
2023-08-29 11:09:22 +0200

    comment tweak

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -76,7 +76,7 @@ async def test_glyphRead(testFont, glyphName):
     glyph = await testFont.getGlyph(glyphName)
     if glyphName == "A" and "com.glyphsapp.glyph-color" not in glyph.customData:
         # glyphsLib doesn't read the color attr from Glyphs-2 files,
-        # so lets monkeypatch the data
+        # so let's monkeypatch the data
         glyph.customData = {"com.glyphsapp.glyph-color": [120, 220, 20, 4]}
     glyphPath = expectedGlyphDataDir / userNameToFileName(glyphName, suffix=".json")
     glyphDict = json.loads(json.dumps(asdict(glyph)))
