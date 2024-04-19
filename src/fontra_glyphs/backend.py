6ac69436b36839b336c18567003571882977f2bf
Olli Meier <hallo@olli-meier.de>
2024-04-19 16:20:11 +0200

    fixing a bad x/y mistake

--- src/fontra_glyphs/backend.py
+++ src/fontra_glyphs/backend.py
@@ -374,9 +374,9 @@ def gsAnchorToFontraAnchor(gsAnchor):
     anchor = Anchor(
         name=gsAnchor.name,
         x=gsAnchor.position.x,
-        y=gsAnchor.position.x,
+        y=gsAnchor.position.y,
         # TODO: gsAnchor.orientation -> Type: int
-        # If the position of the anchor is relaive to the LSB (0), center (2) or RSB (1).
+        # If the position of the anchor is relative to the LSB (0), center (2) or RSB (1).
         customData=gsAnchor.userData if gsAnchor.userData else dict(),
     )
     return anchor
